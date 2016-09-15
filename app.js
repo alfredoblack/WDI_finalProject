00ffadb5a943031f9a0060ee35b7386352915a63
Adriana Black <adrianablancov@gmail.com>
2016-09-15 10:16:10 +0100

    Added socket.io for a live twitter feed

--- app.js
+++ app.js
@@ -6,45 +6,6 @@ var cors = require('cors');
 var bodyParser = require('body-parser');
 var bluebird = require('bluebird');
 var jwt = require('jsonwebtoken');
-var Twit = require('twit');
-
-var passwords = require('./config/tokens.js');
-console.log(passwords);
-
-// var T = new Twit(passwords);
-
-// var params = { 
-//   q: 'rainbow', 
-//   count: 2 
-// };
-
-// T.get('search/tweets', params, gotData);
-//   function gotData(err, data, response) {
-//     console.log(data)
-//     // var tweets = data.statuses
-//     // for(var i = 0; i< tweets.length; i++){
-//     //   console.log(tweets[i].text);
-//     // }
- 
-// };
-
-// // function makeTweet(){
-// //   var tweet = {
-// //     status: 'hello world! this tweet is brought to you from node.js!'
-// //   }
-
-// //   T.post('statuses/update',tweet, tweeted);
-
-// //   function tweeted(err, data, response){
-// //     if(err){
-// //       console.log('something went wrong ...' + err);
-// //     } else {
-// //       console.log(data);
-// //     }
-    
-// //   }
-// // }
-
 
 var request = require('request-promise');
 var qs = require('qs');
@@ -74,10 +35,30 @@ app.use(express.static('public'));
 
 app.use('/api', routes);
 
-
-
-app.listen(port, function(){
+var server = app.listen(port, function(){
   console.log("express is working on " + port);
 });
+
+// This is needed for sockets
+var io = require("socket.io")(server);
+var Twit = require("twit");
+var twitter = new Twit({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+var stream = twitter.stream("statuses/filter", {track: ["dodgeball", "generalassembly", "#code"]});
+io.on("connect", function(socket){
+  stream.on("tweet", function(tweet){
+    var data = {};
+    data.name = tweet.user.name;
+    data.text = tweet.text;
+    data.user_profile_image = tweet.user.profile_image_url;
+    socket.emit("tweets", data);
+  });
+});
+
 
 module.exports = app;
--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -3,8 +3,8 @@ angular
   .controller('MainController', MainController);
 
 
-  MainController.$inject = ['$auth', '$state', '$rootScope', 'Player'];
-  function MainController($auth, $state, $rootScope, Player) {
+  MainController.$inject = ['$auth', '$state', '$rootScope', 'Player', '$window'];
+  function MainController($auth, $state, $rootScope, Player, $window) {
     var self = this;
 
     this.currentUser = $auth.getPayload();
@@ -40,5 +40,19 @@ angular
       self.currentUser = null;
       $state.go("main");
     }
+
+
+    var socket = $window.io();
+
+    socket.on("connect", function(){
+      console.log("connected");
+    });
+
+    var counter = 0;
+
+    socket.on("tweets", function(tweet){
+      $('#tweetbox').prepend('<div class="row"><div class="col-md-2"><img src="' + tweet.user_profile_image + '" class="avatar pull-left"/></div><div class="col-md-2"><div class="names"><span class="full-name"><strong>@' + tweet.name + ' </strong></span></div></div><div class="col-md-8"><div class="contents"><span class="text">' + tweet.text + '</span></div></div></div><br>');
+         // console.log(tweet.text, tweet.name, tweet.user_profile_image);
+    });
 
   }
