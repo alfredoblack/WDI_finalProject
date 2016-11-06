a43a579f3dde7991d1b1b0213fe44cd679558371
Adriana Black <adrianablancov@gmail.com>
2016-11-06 15:14:58 +0000

    removing sockets to test deployment

--- app.js
+++ app.js
@@ -40,25 +40,25 @@ var server = app.listen(port, function(){
 });
 
 // This is needed for sockets
-var io = require("socket.io")(server);
-var Twit = require("twit");
-var twitter = new Twit({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token: process.env.TWITTER_ACCESS_TOKEN,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-});
+// var io = require("socket.io")(server);
+// var Twit = require("twit");
+// var twitter = new Twit({
+//   consumer_key: process.env.TWITTER_CONSUMER_KEY,
+//   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+//   access_token: process.env.TWITTER_ACCESS_TOKEN,
+//   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+// });
 
-var stream = twitter.stream("statuses/filter", {track: ["dodgeball", "junior developers", "#juniordevelopers"]});
-io.on("connect", function(socket){
-  stream.on("tweet", function(tweet){
-    var data = {};
-    data.name = tweet.user.name;
-    data.text = tweet.text;
-    data.user_profile_image = tweet.user.profile_image_url;
-    socket.emit("tweets", data);
-  });
-});
+// var stream = twitter.stream("statuses/filter", {track: ["dodgeball", "junior developers", "#juniordevelopers"]});
+// io.on("connect", function(socket){
+//   stream.on("tweet", function(tweet){
+//     var data = {};
+//     data.name = tweet.user.name;
+//     data.text = tweet.text;
+//     data.user_profile_image = tweet.user.profile_image_url;
+//     socket.emit("tweets", data);
+//   });
+// });
 
 
 module.exports = app;
